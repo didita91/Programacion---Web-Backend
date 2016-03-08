5d9d9e5335904851cccc24805f5e2789d631ee3c
didita91 <didif.91@gmail.com>
2016-03-08 01:14:30 -0300

    cambios ultimos

--- ejb/src/main/java/py/pol/una/ii/pw/service/ClienteService.java
+++ ejb/src/main/java/py/pol/una/ii/pw/service/ClienteService.java
@@ -1,60 +1,38 @@
 package py.pol.una.ii.pw.service;
 
+import java.util.ArrayList;
+
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 import py.pol.una.ii.pw.beans.ClienteManager;
 import py.pol.una.ii.pw.model.Cliente;
+
 @Stateless
 public class ClienteService {
 	@Inject
 	private ClienteManager clienteManager;
-	
-	//public static final ArrayList<Cliente> clienteList = null;
-	//public static final Iterator<Cliente> clienteIterator = clienteList
-			//.iterator();
 
 	public void crear(Cliente cliente) throws Exception {
 		clienteManager.create(cliente);
 	}
 
-	/*public void modificar(Cliente clienteActual, Cliente clienteNuevo)
-			throws Exception {
-
-		while (clienteIterator.hasNext()) {
-			Cliente cliente = clienteIterator.next();
-			if (cliente.equals(clienteActual)) {
-				cliente = clienteNuevo;
-				break;
-			}
-		}
+	public void modificarCliente(Integer id, Cliente entity) {
+		clienteManager.edit(entity);
 	}
 
-	public void eliminar(String clienteNombre) throws Exception {
+	public void eliminar(Integer idCliente) throws Exception {
 
-		int i = 0;
-		while (clienteIterator.hasNext()) {
-			Cliente c = clienteIterator.next();
-			if (c.getNombre().equals(clienteNombre)) {
-				clienteList.remove(i);
-				break;
-			}
-			i++;
-		}
-	}
-
-	public Cliente buscar(String clienteNombre) throws Exception {
-		while (clienteIterator.hasNext()) {
-			Cliente c = clienteIterator.next();
-			if (c.getNombre().equals(clienteNombre)) {
-				return c;
-			}
-		}
-		return null;
+		clienteManager.remove(clienteManager.find(idCliente));
 	}
 
 	public ArrayList<Cliente> listar() throws Exception {
 
-		return clienteList;
-	}*/
+		return (ArrayList<Cliente>) clienteManager.findAll();
+	}
+
+	public Cliente find(Integer id) {
+		return clienteManager.find(id);
+	}
+
 }
--- web/src/main/java/py/pol/una/ii/pw/rest/ClienteResources.java
+++ web/src/main/java/py/pol/una/ii/pw/rest/ClienteResources.java
@@ -1,9 +1,16 @@
 package py.pol.una.ii.pw.rest;
 
+import java.util.ArrayList;
+
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
 
 import py.pol.una.ii.pw.model.Cliente;
 import py.pol.una.ii.pw.service.ClienteService;
@@ -17,47 +24,37 @@ public class ClienteResources {
 @Path("/creacion")
 @Consumes("application/json")
 public void crearCliente(Cliente cliente) throws Exception{
-	//Cliente cliente = new Cliente();
-	//cliente.setNombre(nombre);cliente.setRuc(ruc);
-	//cliente.setSaldo(Integer.parseInt(saldo));
+
 	clienteService.crear(cliente);
-	//return Response.ok("Se creo exitosamente").build();
 }
 
-//@PUT
-//@Path("/modificacion")
-//public Response modificarCliente(@QueryParam("nombre") String nombre,@QueryParam("ruc") String ruc, @QueryParam("saldo") String saldo,@QueryParam("nombre") String nombreNuevo,@QueryParam("ruc") String rucNuevo, @QueryParam("saldo") String saldoNuevo) throws Exception{
-//	Cliente clienteActual = new Cliente();
-//	clienteActual.setNombre(nombre);
-//	clienteActual.setRuc(ruc);
-//	clienteActual.setSaldo(Integer.parseInt(saldo));
-//	Cliente clienteNuevo= new Cliente();
-//	clienteNuevo.setNombre(nombreNuevo);
-//	clienteNuevo.setRuc(rucNuevo);
-//	clienteNuevo.setSaldo(Integer.parseInt(saldo));
-//	clienteService.modificar(clienteActual, clienteNuevo);
-//	
-//	return Response.ok("Se modifico exitosamente").build();
-//}
-//
-//@GET
-//@Path("/")
-//public Response listarCliente() throws Exception{
-//	ArrayList<Cliente> cliente = clienteService.listar();
-//	return Response.ok(cliente).build();
-//}
-//
-//@GET
-//@Path("/busqueda")
-//public Response buscarCliente(@QueryParam("clienteNombre") String clienteNombre) throws Exception{
-//	Cliente cliente = clienteService.buscar(clienteNombre);
-//	return Response.ok(cliente).build();
-//}
-//
-//@DELETE
-//@Path("/eliminacion")
-//public Response eliminarCliente(@QueryParam("clienteNombre") String clienteNombre) throws Exception{
-//	clienteService.eliminar(clienteNombre);
-//	return Response.ok("Se elimino exitosamente").build();
-//}
+@PUT
+@Path("{id}")
+public Response modificarCliente(@PathParam("id") Integer idCliente,Cliente cliente) throws Exception{
+	clienteService.modificarCliente(idCliente,cliente);
+	return null;
+
+
+}
+
+@DELETE
+@Path("{id}")
+public void remove(@PathParam("id") Integer id) throws Exception {
+	clienteService.eliminar(id);
+}
+
+@GET
+@Path("/")
+public Response listarCliente() throws Exception{
+	ArrayList<Cliente> cliente = clienteService.listar();
+	return Response.ok(cliente).build();
+}
+
+@GET
+@Path("{id}")
+public Response buscarCliente(@PathParam("id") Integer idCliente) throws Exception{
+	Cliente cliente = clienteService.find(idCliente);
+	return Response.ok(cliente).build();
+}
+
 }
